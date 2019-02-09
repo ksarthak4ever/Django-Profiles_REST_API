eab3d37c25b32f92ce65e32eeeaaad8a1065e890
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-02-10 01:03:10 +0530

    created first APIView(testing)

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView 
+from rest_framework.response import Response
+
+
+class HelloApiView(APIView):
+	""" Testing API View. """
+
+	def get(self,request,format=None):
+		"Returns a list of APIView features."
+
+		an_apiview = [
+			'Hello',
+			'World',
+			'Testing',
+			'ksarthak4ever'
+		]
+
+		return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+
