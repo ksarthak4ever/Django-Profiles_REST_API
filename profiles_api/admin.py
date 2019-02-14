b1cc3687249abfbfbd9b992eecd3fad8956a024c
Sarthak Kumar <ksarthak4ever@gmail.com>
2019-02-14 05:57:10 +0530

    created model for profile feed items

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.UserProfile) #from admin.site module importing register function and passing our UserProfile model
+admin.site.register(models.ProfileFeedItem) #registering Profile feed model
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -49,3 +49,12 @@ class UserProfile(AbstractBaseUser, PermissionsMixin): #Used to represent a user
 		return self.email
 
 
+
+class ProfileFeedItem(models.Model): #profile status/feed update
+
+	user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+	status_text = models.CharField(max_length=250)
+	created_on = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self): #return the model as a string
+		return self.status_text
